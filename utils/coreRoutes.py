2a3bc37d961a3ac4792e71982805393084744b14
GuilhermeOth <guilhermeoth.pestana@gmail.com>
2024-05-13 22:00:21 -0300

    refactor: coreRoutes URLs variable

--- utils/coreRoutes.py
+++ utils/coreRoutes.py
@@ -1,24 +1,27 @@
 import requests
 
+BASE_URL = 'http://localhost:5000/api/v1'
+
+
 def createKnowledgeBase(assistantName):
-    url = f'http://localhost:5000/api/v1/knowledge-base/{assistantName}'
+    url = f'{BASE_URL}/knowledge-base/{assistantName}'
     response = requests.post(url, '''docFilePath''')  # TODO get docFilePath by assistantName
     return response
 
 
 def initSession(assistantName):
-    url = f'http://localhost:5000/api/v1/llm/{assistantName}'
+    url = f'{BASE_URL}/llm/{assistantName}'
     response = requests.post(url, '''promptFilePath''')   #TODO get promptFilePath by assistantName
     return response
 
 def sendCoreChat(assistantName, payload):
-    url = f'http://localhost:5000/api/v1/llm/query/{assistantName}'
+    url = f'{BASE_URL}/llm/query/{assistantName}'
     response = requests.post(url, payload)
     return response
 
 
 def closeSession(assistantName):
-    url = f'http://localhost:5000/api/v1/llm/{assistantName}'
+    url = f'{BASE_URL}/llm/{assistantName}'
     response = requests.delete(url)
     return response
 
